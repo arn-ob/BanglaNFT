c3001aeac2c3dc70b0ca2793000d9e033d34ff17
Arnob Almazee <arnobxtreme@gmail.com>
2022-10-29 13:40:32 +0600

    _

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-etherscan")
 require("hardhat-deploy")
 require("solidity-coverage")
 require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
+require("hardhat-contract-sizer") 
 require("dotenv").config()
 
 /**
